5d0bfd260bacd71aa0cec26bfa2a7f62a4f0cb2e
unknown <travihay@gmail.com>
2024-09-22 15:24:38 -0500

    added plugs  and css loader to webpack.config and added database.js logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from 'idb';
 
+// Initialize the IndexedDB
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -12,10 +13,48 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic to add content to the database
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  
+  // Open the jate database
+  const jateDb = await openDB('jate', 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Create a transaction with readwrite access
+  const tx = jateDb.transaction('jate', 'readwrite');
 
+  // Open the jate object store
+  const store = tx.objectStore('jate');
+
+  // Add or update the content in the object store
+  const request = store.put({ content });
+
+  // Get the result from the request
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+// Logic to retrieve all content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Open the jate database
+  const jateDb = await openDB('jate', 1);
+
+  // Create a transaction with readonly access
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Open the jate object store
+  const store = tx.objectStore('jate');
+
+  // Get all the content from the object store
+  const request = store.getAll();
+
+  // Get the result from the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+// Initialize the database
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,27 +3,68 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'development', // Set the mode to development
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.bundle.js', // Output for different entry points
+      path: path.resolve(__dirname, 'dist'), // Output path
     },
     plugins: [
-      
+      // Plugin to generate an HTML file
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'PWA App'
+      }),
+
+      // Inject the custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js', // Custom service worker file path
+        swDest: 'service-worker.js', // Output service worker file
+      }),
+
+      // Create a manifest.json for the PWA
+      new WebpackPwaManifest({
+        name: "PWA App",
+        short_name: "PWA",
+        description: "A progressive web app setup example",
+        background_color: '#ffffff',
+        theme_color: '#317EFB',
+        display: 'standalone',
+        start_url: './', // App start page
+        crossorigin: 'use-credentials', // Ensure credentials mode is used
+        icons: [
+          {
+            src: path.resolve('src/assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // Various icon sizes
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS loader
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'], // Loaders for CSS files
+        },
+        // Babel loader for JS
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'], // Preset for modern JavaScript
+            },
+          },
+        },
       ],
     },
   };
